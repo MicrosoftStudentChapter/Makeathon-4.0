1dfc2b6f9d915683d816e1c62019203ec0759787
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-01-24 22:33:00 +0530

    Landing page nav bugs removed

--- Assets/Landing Page/assets/js/navbar.js
+++ Assets/Landing Page/assets/js/navbar.js
@@ -1,23 +1,21 @@
-
 var isOpen = false;
 
-const btn = document.getElementById("openbtn123")
+const btn = document.getElementById("openbtn123");
 
 function openNav() {
-    
-    const width = screen.width;
-    btn.classList.add("hide-item")
-    if(width <= 1024){
-        document.getElementById("mySidebar").style.width = "50%";
-        document.getElementById("main").style.marginLeft = "50%";
-    }else{
-        document.getElementById("mySidebar").style.width = "20%";
-        document.getElementById("main").style.marginLeft = "20%";
-    }
+  const width = screen.width;
+  btn.classList.add("hide-item");
+  if (width <= 1024) {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+  } else {
+    document.getElementById("mySidebar").style.width = "20%";
+    document.getElementById("main").style.marginLeft = "20%";
+  }
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    btn.classList.remove("hide-item")
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  btn.classList.remove("hide-item");
 }
